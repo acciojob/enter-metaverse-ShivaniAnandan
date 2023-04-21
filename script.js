5f185da8a19912c4a9bf2d1e02eea126271498ac
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-04-21 20:35:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function enter(){
+	document.getElementById("status").innerHTML="<h1>Entered Metaverse</h1>";
+	
+}
+var btn=document.getElementById("enterBtn");
+btn.addEventListener("click",enter);
